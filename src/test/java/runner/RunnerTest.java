2b5524f133630788a31b11250a1b8f913c19483e
Mona Lisa <Mona Lisa>
2025-04-03 15:03:35 +0200

    runner modif

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = {"steps","features"},
 
-        plugin = {"pretty", "html:target/cucumber-reports.html"},
+        plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json"},
 
         tags = "@TNR"
 )
