424876511596c1f18b08693800e4abf72f2f3463
Mona Lisa <Mona Lisa>
2025-04-03 11:04:53 +0200

    runner modif

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -7,8 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "steps",
-        dryRun = true ,
+        glue = {"steps","features"},
 
         plugin = {"pretty", "html:target/cucumber-reports.html"},
 
